51fec99d53fdc94d33abd47bb441c8bc9d7e64ee
Trang Nguyen <ntttrang2130@gmail.com>
2022-02-28 22:34:07 -0500

    merge sort

--- JS-Masterclass/Algorithms/Sorting/Detail_Notes.md
+++ JS-Masterclass/Algorithms/Sorting/Detail_Notes.md
@@ -185,11 +185,13 @@ Builds up the sort by gradually creating a larger left half which is always sort
   - If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
   - Once we exhaust one array, push in all remaining values from the other array
 
-**mergeSort Pseudocode**
+1. mergeSort Pseudocode
 
 - Break up the array into halves until you have arrays that are empty or have one element
 - Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
 - Once the array has been merged back together, return the merged (and sorted!) array
+
+2. [mergeSort code](Merge_Sort.js)
 
 #### Merge Sort BIG O
 
---
+++ JS-Masterclass/Algorithms/Sorting/Insertion_Sort.js
@@ -0,0 +1,33 @@
+
+// My approach
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//       // Start comparing current element with every element before it
+//       for (let j = i - 1; j > -1; j--) {
+//           // Swap elements as required
+//           console.log("SWAP", arr);
+//           if (arr[j + 1] < arr[j]) {
+//               [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+//           }
+//       }
+//   }
+//   return arr;
+// }
+
+// Colt's solution
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+      let currentVal = arr[i];
+      let j;
+      for (j = i - 1; j > -1 && arr[j] > currentVal; j--) {
+        if (arr[j] > currentVal) {
+          arr[j + 1] = arr[j];
+        }
+      }
+      arr[j + 1] = currentVal;
+      // console.log("SWAP", arr);
+    }
+  return arr;
+}
+
+// console.log(insertionSort([2,1,9,76,4]));
