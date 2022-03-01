9e69be0e5b7da56d1555d93625cc870ef8144fda
Trang Nguyen <ntttrang2130@gmail.com>
2022-03-01 12:37:07 -0500

    tuesday study

--- JS-Masterclass/Algorithms/Sorting/Insertion_Sort.js
+++ JS-Masterclass/Algorithms/Sorting/Insertion_Sort.js
@@ -32,3 +32,15 @@ function insertionSort(arr) {
 
 // console.log(insertionSort([2,1,9,76,4]));
 
+//OPTIMIZED VERSION
+function insertionSort(arr){
+	var currentVal;
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal;
+    }
+    return arr;
+}
